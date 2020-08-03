a9720093f8b3f12e40d3339d6051c1fbd1bd328a
happyeven <1361840595@qq.com>
2020-08-04 00:00:47 +0800

    [react basic] [Dong] refactor : create store in index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux'
+import CounterGroup from './component/CounterGroup';
+import counter from './reducers'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+const store = createStore(counter)
+const rootEl = document.getElementById('root')
+const render = () => ReactDOM.render(
+  <CounterGroup
+    // value={store.getState()}
+    // onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+    // onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+    store = {store}
+  />,
+  rootEl
+)
+render()
+store.subscribe(render)
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

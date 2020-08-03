8cee574746da4e23b01819125aff67003672d2c3
happyeven <1361840595@qq.com>
2020-08-04 00:02:51 +0800

    [react basic] [Dong] refactor : add props store in Counter

--- src/component/Counter/index.jsx
+++ src/component/Counter/index.jsx
@@ -1,35 +1,49 @@
 import React from 'react';
+import { createStore } from 'redux'
+import counter from '../../reducers'
 
 class Counter extends React.Component {
     constructor(props) {
         super(props)
+        console.log(props)
+        this.store = props.store
         this.state = { value: 0, groupSize: 0 }
     }
 
-    onIncrease = () => {
+    handleIncrease = () => {
         this.setState(prevState => ({ value: prevState.value + 1 }))
-        this.props.onIncrease()
+        this.props.handleIncrease()
+        this.store.dispatch({ type: 'INCREMENT' })
     }
-    onDecrease = () => {
+    handleDecrease = () => {
         this.setState(prevState => ({ value: prevState.value - 1 }))
-        this.props.onDecrease()
+        this.props.handleDecrease()
+        this.store.dispatch({ type: 'DECREMENT' })
     }
     render() {
         return <div>
-            <button onClick={this.onDecrease}>-</button>
+            <button onClick={this.handleDecrease}>-</button>
             <mark>{this.state.value}</mark>
-            <button onClick={this.onIncrease}>+</button>
+            <button onClick={this.handleIncrease}>+</button>
         </div>
     }
 
-    static getDerivedStateFromProps(props, state) {
-        if (props.groupSize !== state.groupSize) {
-            return {
-                groupSize: props.groupSize,
-                value: 0
-            }
+    // static getDerivedStateFromProps(props, state) {
+    //     if (props.groupSize !== state.groupSize) {
+    //         return {
+    //             groupSize: props.groupSize,
+    //             value: 0
+    //         }
+    //     }
+    //     return null
+    // }
+
+     componentWillUnmount() {
+        console.log("come in ")
+        if (this.state.value > 0) {
+            this.store.dispatch({ type: 'DECREMENT' })
         }
-        return null
+        this.store.dispatch({ type: 'INCREMENT' })
     }
 }
 
